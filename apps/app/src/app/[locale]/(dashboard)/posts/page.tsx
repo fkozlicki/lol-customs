bbed7d583d974f52e443299ca06d4982bbd3be59
fkozlicki <fk@tonik.com>
2026-03-15 12:59:28 +0100

    refactor: created PostListSkeleton component

--- apps/app/src/app/[locale]/(dashboard)/posts/page.tsx
+++ apps/app/src/app/[locale]/(dashboard)/posts/page.tsx
@@ -1,18 +1,8 @@
-import { Skeleton } from "@v1/ui/skeleton";
 import { Suspense } from "react";
 import { PostList } from "@/components/forum/post-list";
+import PostListSkeleton from "@/components/forum/post-list-skeleton";
 import { getScopedI18n } from "@/locales/server";
 import { HydrateClient, prefetch, trpc } from "@/trpc/server";
-
-function PostListSkeleton() {
-  return (
-    <div className="space-y-3">
-      {Array.from({ length: 5 }).map((_, i) => (
-        <Skeleton key={i} className="h-24 w-full rounded-lg" />
-      ))}
-    </div>
-  );
-}
 
 export default async function PostsPage() {
   const t = await getScopedI18n("dashboard.pages.posts");
---
+++ apps/app/src/components/forum/post-list-skeleton.tsx
@@ -0,0 +1,11 @@
+import { Skeleton } from "@v1/ui/skeleton";
+
+export default function PostListSkeleton() {
+  return (
+    <div className="space-y-3">
+      {Array.from({ length: 5 }).map((_, i) => (
+        <Skeleton key={i} className="h-24 w-full rounded-lg" />
+      ))}
+    </div>
+  );
+}
